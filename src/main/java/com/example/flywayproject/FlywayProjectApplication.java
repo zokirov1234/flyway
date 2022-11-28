1ba6283ef5d3ec501991f6595314018a4ce3b5f1
Jahongir <zokirov123404321@gmail.com>
2022-11-28 19:30:59 +0500

    Initial commit

---
+++ src/main/java/com/example/flywayproject/FlywayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flywayproject/FlywayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flywayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
